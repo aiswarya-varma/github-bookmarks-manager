d215f1ad44b2e5d22eec62452e79d6a2d8c55f6b
aiswarya-varma <aiswaryavarma44@gmail.com>
2020-11-28 23:42:42 +0530

    Comment, new styles

--- src/components/DefaultView.js
+++ src/components/DefaultView.js
@@ -1,15 +1,19 @@
+/**
+ * Fallback view to be displayed when there are no bookmarked repositories
+ */
+
 import React from 'react';
 import { Row } from 'react-bootstrap';
 import AddBookmarkButton from './AddBookmarkButton';
 
 const DefaultView = () => {
     return (
-        <>
+        <div className="default-view">
             <Row className="justify-content-md-center font-medium">Oops! No Bookmarked Repositories</Row>
             <Row className="justify-content-md-center">
                 <AddBookmarkButton text="Add Repositories to Bookmarks" />
             </Row>
-        </>
+        </div>
     );
 }
 
